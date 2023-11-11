1eca6ae1314778191a4d6337c401011d7ac632dc
Blazej Leman <blazejleman789@gmail.com>
2023-11-11 22:04:32 +0100

    format

--- map.cpp
+++ map.cpp
@@ -2,18 +2,17 @@
 #include <string>
 #include <map>
 
-void print(const std::map<size_t, std::string>& map) {
-    for (const auto& pair : map) {
+void print(const std::map<size_t, std::string> &map) {
+    for (const auto &pair : map) {
         std::cout << pair.first << " | " << pair.second << '\n';
     }
 }
 
 void mapa() {
-    std::map<size_t, std::string> discs {
+    std::map<size_t, std::string> discs{
         {1, "The Cat"},
         {2, "The Dog"},
-        {3, "The Duck"}
-    };
+        {3, "The Duck"}};
     print(discs);
     std::cout << "\n After adding a new element\n";
     discs[4] = "Harry Potter";
